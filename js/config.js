d0d2408ee72f9028574e8e001bb20eb14f4b97dd
MediQuoBrasil <vinicius.vilela@mediquo.com.br>
2026-06-05 14:15:28 -0300

    Update config.js

--- js/config.js
+++ js/config.js
@@ -6,7 +6,7 @@
  */
 
 /** @const {string} URL do Web App do Google Apps Script (terminada em /exec). */
-const API_URL = 'https://script.google.com/macros/s/COLE_O_DEPLOY_ID_AQUI/exec';
+const API_URL = 'https://script.google.com/macros/s/AKfycbzAbzXdR0TdLpc1PM-xcEeQz6lfOY7U9hALud2bGazXv8JpApBn7M9lS-aj7fTWgCGxjQ/exec';
 
 /** @const {string} Client ID do Google Cloud para Sign In with Google. */
-const GIS_CLIENT_ID = 'COLE_O_CLIENT_ID_AQUI.apps.googleusercontent.com';
+const GIS_CLIENT_ID = '970946070001-qs7qn28ariinkf77p0v951mf819pso67.apps.googleusercontent.com';
